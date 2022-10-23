297e2b11eabb137d24a2e1a21334fd1e689274c5
tileb <tim.lebailly@gmail.com>
2022-10-23 14:28:42 +0200

    test model

---
+++ job.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Parameters
+#SBATCH --account=2022_017
+#SBATCH --gpus-per-node=1
+#SBATCH --cpus-per-gpu=12
+#SBATCH --mem=62GB
+#SBATCH --nodes=1
+#SBATCH --open-mode=append
+#SBATCH --partition=gpu_rome_a100
+#SBATCH --time=120
+
+
+echo $(which conda)
+python few_shot.py --n-way 5 --n-support 1 --model $model_name --dataset $dataset_name
---
+++ run_all.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sbatch job --export=model_name='deepcluster-v2' --export=dataset_name='dtd'
